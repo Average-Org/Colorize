c30eef25bcd44634fd19310377d99cb3d4c94f43
Julian <julianmuchacho666@gmail.com>
2023-12-04 23:59:26 -0500

    Very simple and basic color formatting tool for TShock

---
+++ Colorize/Extensions/TSPlayer.Extensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using TShockAPI;
+
+namespace Colorize
+{
+    public static class TSPlayerExtensions
+    {
+        public static void Error(this TSPlayer player, string text)
+        {
+            player.SendErrorMessage(text);
+        }
+
+        public static void Info(this TSPlayer player, string text)
+        {
+            player.SendInfoMessage(text);
+        }
+
+        public static void Msg(this TSPlayer player, string text)
+        {
+            player.SendMessage(text, Color.White);
+        }
+
+        public static void Msg(this TSPlayer player, string text, Color color)
+        {
+            player.SendMessage(text, color);
+        }
+
+        public static void MsgGradient(this TSPlayer player, string text, Color color, Color otherColor)
+        {
+            player.SendMessage(Tools.GradientText(text, color, otherColor), Color.White);
+        }
+    }
+}
---
+++ Colorize/Tools.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using System.Text;
+using Terraria;
+using Terraria.ID;
+
+namespace Colorize
+{
+    public static class Tools
+    {
+        public static string ColorizeText(string text, Color color)
+        {
+            return $"[c/{color.Hex3()}:{text}]";
+        }
+
+        public static string GradientText(string text, Color color, Color otherColor)
+        {
+            StringBuilder gradientedText = new();
+
+            var index = 0;
+            foreach (char c in text)
+            {
+                float ratio = (float)index / (text.Length - 1);
+                Color thisColor = Color.Lerp(color, otherColor, ratio);
+                gradientedText.Append($"[c/{thisColor.Hex3()}:{c}]");
+            }
+            return gradientedText.ToString();
+        }
+
+        public static string ItemIcon(Item item)
+        {
+            return ItemIcon(item.type);
+        }
+
+        public static string ItemIcon(ItemID itemID)
+        {
+            return ItemIcon(itemID);
+        }
+
+        public static string ItemIcon(int itemID)
+        {
+            return $"[i/{itemID}]";
+        }
+    }
+}
